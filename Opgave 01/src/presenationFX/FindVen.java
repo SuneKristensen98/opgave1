86515b20073d930785f4eada6bc162ad47d81351
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2018-10-03 12:37:06 +0200

    javafx2

--- Opgave 01/src/presenationFX/FindVen.java
+++ Opgave 01/src/presenationFX/FindVen.java
@@ -1,5 +1,37 @@
 package presenationFX;
+import presenationFX.HovedMenu;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class FindVen {
+public class FindVen extends Application {
 
+	Label response;
+	TextField tf;
+	Stage window;
+	Scene Hovedmenu, Opret, Opdatere, Slet, Hent, Find;
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		Label label5 = new Label("Find Ven eller bekendt");
+		Button btnBack4 = new Button("Gå tilbage til hovedmenuen");
+		btnBack4.setPrefSize(170, 10);
+		btnBack4.setOnAction(e -> window.setScene(Hovedmenu));
+		VBox layout5 = new VBox(20);
+		layout5.setAlignment(Pos.CENTER);
+		layout5.getChildren().addAll(label5, btnBack4);
+		Find = new Scene(layout5, 800, 600);
+		
+		
+}
 }
--- Opgave 01/src/presenationFX/HovedMenu.java
+++ Opgave 01/src/presenationFX/HovedMenu.java
@@ -7,14 +7,17 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HovedMenu extends Application {
 	Label response;
-
+	TextField tf;
 	Stage window;
 	Scene Hovedmenu, Opret, Opdatere, Slet, Hent, Find;
 
@@ -49,45 +52,55 @@ public class HovedMenu extends Application {
 		window.setScene(Hovedmenu);
 		window.show();
 
-
-		
 		Label label1 = new Label("Opret Ven");
 		Button btnBack = new Button("Gå tilbage til hovedmenuen");
-		btnBack.setPrefSize(100, 10);
+		btnBack.setPrefSize(170, 10);
 		btnBack.setOnAction(e -> window.setScene(Hovedmenu));
 		VBox layout1 = new VBox(20);
 		layout1.setAlignment(Pos.CENTER);
 		layout1.getChildren().addAll(label1, btnBack);
 		Opret = new Scene(layout1, 800, 600);
 
-		
-
 		Label label2 = new Label("Opdatere Ven eller bekendt");
 		Button btnBack1 = new Button("Gå tilbage til hovedmenuen");
-		btnBack1.setPrefSize(100, 10);
+		btnBack1.setPrefSize(170, 10);
 		btnBack1.setOnAction(e -> window.setScene(Hovedmenu));
 		VBox layout2 = new VBox(20);
+		layout2.setAlignment(Pos.CENTER);
 		layout2.getChildren().addAll(label2, btnBack1);
 		Opdatere = new Scene(layout2, 800, 600);
 
+		Label label3 = new Label("Slet Ven eller bekendt");
+		Button btnBack2 = new Button("Gå tilbage til hovedmenuen");
+		btnBack2.setPrefSize(170, 10);
+		btnBack2.setOnAction(e -> window.setScene(Hovedmenu));
+		VBox layout3 = new VBox(20);
+		layout3.setAlignment(Pos.CENTER);
+		layout3.getChildren().addAll(label3, btnBack2);
+		Slet = new Scene(layout3, 800, 600);
 
+		Label label4 = new Label("Hent Ven eller bekendt");
+		Button btnBack3 = new Button("Gå tilbage til hovedmenuen");
+		btnBack3.setPrefSize(170, 10);
+		btnBack3.setOnAction(e -> window.setScene(Hovedmenu));
+		VBox layout4 = new VBox(20);
+		layout4.setAlignment(Pos.CENTER);
+		layout4.getChildren().addAll(label4, btnBack3);
+		Hent = new Scene(layout4, 800, 600);
+
+		Label label5 = new Label("Find Ven eller bekendt");
+		Button btnBack4 = new Button("Gå tilbage til hovedmenuen");
+		Label findtext = new Label ("Indtast din email her");
+		tf = new TextField();
+		btnBack4.setPrefSize(170, 10);
+		btnBack4.setOnAction(e -> window.setScene(Hovedmenu));
+		VBox layout5 = new VBox(20);
+		layout5.setAlignment(Pos.CENTER);
+		layout5.getChildren().addAll(label5, findtext, btnBack4, tf);
+		Find = new Scene(layout5, 800, 600);
 		
-//		Label label3 = new Label("Slet Ven eller bekendt");
-//		VBox layout3 = new VBox(20);
-//		layout3.getChildren().addAll(label3, btnSlet);
-//		Slet = new Scene(layout3, 800, 600);
-//
-//		
-//		Label label4 = new Label("Hent Ven eller bekendt");
-//		VBox layout4 = new VBox(20);
-//		layout4.getChildren().addAll(label4, btnHent);
-//		Hent = new Scene(layout4, 800, 600);
-//
-//		
-//		Label label5 = new Label("Find Ven eller bekendt");
-//		VBox layout5 = new VBox(20);
-//		layout5.getChildren().addAll(label5, btnFind);
-//		Find = new Scene(layout5, 800, 600);
+	
+				
 
 	}
 
